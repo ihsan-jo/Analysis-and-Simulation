bf9deb1f17d7fdb9c8ca6d7a9c622ce118cc6231
Ihsan M. Fauzan <ihsanmf.work@gmail.com>
2026-02-12 13:57:35 +0400

    Make Separate Program

---
+++ StateSpace.m
@@ -0,0 +1,12 @@
+Kr = 10000;      % N/m
+Kv = 10000;      % N/m
+Ko = 0;          % N/m
+
+J  = 0.0013;     % kg*m^2
+L  = 0.06;       % m (converted from 6 cm)
+
+br = 2.89;       % N*s/m
+bo = 2.89;       % N*s/m
+
+% state space
+A = 
